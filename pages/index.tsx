945cd3f4b08a8f130cfb47df2b03b40dcd23779d
Maxi <maximilianoaibarra@gmail.com>
2023-01-29 13:40:27 -0300

    Added react-swipeable-views for carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Image from 'next/image'
-import Grid from '@mui/material/Grid';
+import Image from "next/image";
+import Grid from "@mui/material/Grid";
 import mainLogo from "../public/images/logo-horizontal-dswhite.png";
+import { useState } from "react";
+import SwipeableViews from "react-swipeable-views";
 
 const GGLogo = () => (
   <Image
@@ -12,17 +14,62 @@ const GGLogo = () => (
 );
 
 export default function Home() {
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
+  const handleChangeIndex = (index: number) => {
+    setCarouselIndex(index);
+  };
+
   return (
-    <Grid
+    <>
+      <Grid
         container
         columns={16}
         height={"20vh"}
         sx={{ position: "absolute", top: 0, left: 0, zIndex: 100 }}
         padding={4}
       >
-        <Grid item xs={8} md={4} lg={4}>
+        <Grid item xs={8} md={4} lg={4} onClick={() => setCarouselIndex(2)}>
           <GGLogo />
         </Grid>
-    </Grid>
-  )
+      </Grid>
+      <SwipeableViews
+        index={carouselIndex}
+        default
+        onChangeIndex={handleChangeIndex}
+        enableMouseEvents
+      >
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          height={"100vh"}
+          padding={0}
+        >
+          <h1>Slide 1</h1>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          height={"100vh"}
+          padding={0}
+        >
+          <h1>Slide 2</h1>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          height={"100vh"}
+          padding={0}
+        >
+          <h1>Slide 3</h1>
+        </Grid>
+      </SwipeableViews>
+    </>
+  );
 }
