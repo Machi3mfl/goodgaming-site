d40e93300e1a80381ed6850873a973f77f642b11
Maximiliano Ibarra <6089438+Machi3mfl@users.noreply.github.com>
2023-02-02 13:59:37 -0300

    Updated asset path

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === "production" ? true : false;
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/goodgaming-site/' : '',
-  basePath: isProd ? '/goodgaming-site/' : '',
+  assetPrefix: '',
+  basePath: '',
 }
 
 module.exports = nextConfig;
